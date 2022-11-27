26eeabf9f6dc19768696cdfede11265c8fc22324
Marina <marra@inbox.ru>
2022-11-27 16:35:07 +0300

    Tesk001

---
+++ Tesk001/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int getNaturalPowerOfNumber(int numberA, int numberB)
+{
+    int NaturalPowerOfNumber = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        NaturalPowerOfNumber = NaturalPowerOfNumber * numberA;
+    }
+    return NaturalPowerOfNumber;
+}
+int getNumberFromUser(string message)
+{
+    int result = 1;
+    Console.Write(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int numberA = getNumberFromUser("Введите натуральное число 1: ");
+int numberB = getNumberFromUser("Введите натуральное число 2: ");
+if (numberA == 0 && numberB == 0)
+{
+    Console.WriteLine("Число 0 не является натуральным числом.");
+}
+else
+{
+int NaturalPowerOfNumber = getNaturalPowerOfNumber(numberA, numberB);
+Console.WriteLine($"{numberA} в степени {numberB} равно  {NaturalPowerOfNumber}");
+}
